be5dda018d97a7308874d9f357381bb7691450b8
Hariae <aehari2010@gmail.com>
2018-11-04 22:15:08 -0800

    CORS Configuration change

--- Lab 2 - 013311701/HomeAway/Backend/index.js
+++ Lab 2 - 013311701/HomeAway/Backend/index.js
@@ -15,7 +15,7 @@ var cors = require('cors');
 var passport = require('passport');
 
 //set up cors
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'http://ec2-18-223-153-77.us-east-2.compute.amazonaws.com:3000', credentials: true }));
 
 //set up session variable
 
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 
 //Allow acceess control headers
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://ec2-18-223-153-77.us-east-2.compute.amazonaws.com:3000');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
